2687d11110aecf6dfc5e7ebf0584997216419fcc
oğuz alp çepni <oguzalpcepni@gmail.com>
2023-06-25 09:55:59 +0300

    yeni bir tablo eklemek istediğimiz zaman add getall gibi fonskiyonları tekrar tekrar yazmayı engellemek için ve Db değiştirmeyi kolaylaştırmak için yapılan işlemler

--- Core/DataAccess/EntityFramework/EfEntityRepositoryBase.cs
+++ Core/DataAccess/EntityFramework/EfEntityRepositoryBase.cs
@@ -1,12 +1,65 @@
-﻿using System;
+﻿using Core.DataAcces;
+using Core.Entites;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Core.DataAccess.EntityFramework
 {
-    internal class EfEntityRepositoryBase
+    public class EfEntityRepositoryBase<TEntity,TContext>: IEntityRepository<TEntity>
+        where TEntity : class,IEntity, new()
+        where TContext : DbContext,new()
     {
+        public void Add(TEntity entity)
+        {                                                                               //IDisposable pattern  implementation of c#
+            using (TContext context = new TContext())                     // bir classı newlediğinde o bellekten garbage collector belli bir zamanda düzenli olarak gelir ve bellekten onu atar  using içerisine yazdığınız nesneler  using bitiminde garbage collectore diyprki beni bellekten at çünkü context nesne biraz masraflı 
+            {
+                var addedEntity = context.Entry(entity);                                // git veri kaynağından  benim gönderdiğim productan bir tanesi eşleştir 
+                addedEntity.State = EntityState.Added;
+                context.SaveChanges();                                                  // kısaca 1. satır referansı yakala 2) o aslında eklenecek bir nesne 3) ekle ve kaydet 
+            }                                                                           // garbage collector topla 
+        }
+
+        public void Delete(TEntity entity)
+        {
+            using (TContext context = new TContext())                     // bir classı newlediğinde o bellekten garbage collector belli bir zamanda düzenli olarak gelir ve bellekten onu atar  using içerisine yazdığınız nesneler  using bitiminde garbage collectore diyprki beni bellekten at çünkü context nesne biraz masraflı 
+            {
+                var deletedEntity = context.Entry(entity);                              // git veri kaynağından  benim gönderdiğim productan bir tanesi eşleştir 
+                deletedEntity.State = EntityState.Deleted;
+                context.SaveChanges();
+            }
+        }
+
+        public TEntity Get(Expression<Func<TEntity, bool>> filter)
+        {
+            using (TContext context = new TContext())
+            {
+                return context.Set<TEntity>().SingleOrDefault(filter); // contex.Set<Product>() -----> bu benim ürünler tablom 
+            }
+        }
+
+        public List<TEntity> GetAll(Expression<Func<TEntity, bool>> filter = null)
+        {
+            using (TContext context = new TContext())
+            {
+                return filter == null
+                    ? context.Set<TEntity>().ToList()
+                    : context.Set<TEntity>().Where(filter).ToList();               // SELECT * FROM PRODUCTS
+            }
+        }
+
+        public void Update(TEntity entity)
+        {
+            using (TContext context = new TContext())                     // bir classı newlediğinde o bellekten garbage collector belli bir zamanda düzenli olarak gelir ve bellekten onu atar  using içerisine yazdığınız nesneler  using bitiminde garbage collectore diyprki beni bellekten at çünkü context nesne biraz masraflı 
+            {
+                var updateEntity = context.Entry(entity);                               // git veri kaynağından  benim gönderdiğim productan bir tanesi eşleştir 
+                updateEntity.State = EntityState.Modified;
+                context.SaveChanges();                                                  // kısaca 1. satır referansı yakala 2) o aslında eklenecek bir nesne 3) ekle ve kaydet 
+            }
+        }
     }
 }
--- DataAccess/concrete/EntitiyFramework/EfProductDal.cs
+++ DataAccess/concrete/EntitiyFramework/EfProductDal.cs
@@ -1,4 +1,5 @@
-﻿using DataAccess.Abstract;
+﻿using Core.DataAccess.EntityFramework;
+using DataAccess.Abstract;
 using Entities.concrete;
 using Microsoft.EntityFrameworkCore;
 using System;
@@ -10,54 +11,8 @@ using System.Threading.Tasks;
 
 namespace DataAccess.concrete.EntitiyFramework
 {
-    public class EfProductDal : IProductDal
+    public class EfProductDal : EfEntityRepositoryBase<Product,NorthwindContext>,IProductDal
     {
-        public void Add(Product entity)
-        {                                                                               //IDisposable pattern  implementation of c#
-            using (NorthwindContext context = new NorthwindContext())                     // bir classı newlediğinde o bellekten garbage collector belli bir zamanda düzenli olarak gelir ve bellekten onu atar  using içerisine yazdığınız nesneler  using bitiminde garbage collectore diyprki beni bellekten at çünkü context nesne biraz masraflı 
-            {
-                var addedEntity = context.Entry(entity);                                // git veri kaynağından  benim gönderdiğim productan bir tanesi eşleştir 
-                addedEntity.State=EntityState.Added;
-                context.SaveChanges();                                                  // kısaca 1. satır referansı yakala 2) o aslında eklenecek bir nesne 3) ekle ve kaydet 
-            }                                                                           // garbage collector topla 
-        }
-
-        public void Delete(Product entity)
-        {
-            using (NorthwindContext context = new NorthwindContext())                     // bir classı newlediğinde o bellekten garbage collector belli bir zamanda düzenli olarak gelir ve bellekten onu atar  using içerisine yazdığınız nesneler  using bitiminde garbage collectore diyprki beni bellekten at çünkü context nesne biraz masraflı 
-            {
-                var deletedEntity = context.Entry(entity);                              // git veri kaynağından  benim gönderdiğim productan bir tanesi eşleştir 
-                deletedEntity.State = EntityState.Deleted;
-                context.SaveChanges();
-            }
-        }
-
-        public Product Get(Expression<Func<Product, bool>> filter)
-        {
-            using (NorthwindContext context = new NorthwindContext())
-            {
-                return context.Set<Product>().SingleOrDefault(filter); // contex.Set<Product>() -----> bu benim ürünler tablom 
-            }
-        }
-
-        public List<Product> GetAll(Expression<Func<Product, bool>> filter = null)
-        {
-            using (NorthwindContext context = new NorthwindContext())
-            {
-                return filter == null 
-                    ? context.Set<Product>().ToList() 
-                    : context.Set<Product>().Where(filter).ToList();               // SELECT * FROM PRODUCTS
-            }
-        }
-
-        public void Update(Product entity)
-        {
-            using (NorthwindContext context = new NorthwindContext())                     // bir classı newlediğinde o bellekten garbage collector belli bir zamanda düzenli olarak gelir ve bellekten onu atar  using içerisine yazdığınız nesneler  using bitiminde garbage collectore diyprki beni bellekten at çünkü context nesne biraz masraflı 
-            {
-                var updateEntity = context.Entry(entity);                               // git veri kaynağından  benim gönderdiğim productan bir tanesi eşleştir 
-                updateEntity.State = EntityState.Modified;  
-                context.SaveChanges();                                                  // kısaca 1. satır referansı yakala 2) o aslında eklenecek bir nesne 3) ekle ve kaydet 
-            }
-        }
+       
     }
 }
