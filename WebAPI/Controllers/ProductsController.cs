2916b3fc681fe25b06a8fdaacb0ffb680039d729
oğuz alp çepni <oguzalpcepni@gmail.com>
2023-07-02 22:26:57 +0300

    started WebAPI

---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,20 @@
+﻿using Entities.concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        public List<Product> get()
+        {
+            return new List<Product>
+            {
+                new Product{ProductId = 1,ProductName="elma"},
+                new Product{ProductId = 2,ProductName="armut"},
+            };
+        }
+    }
+}
