8f0c1fde8997ff873602d16b939d68d9e2df38e8
oğuz alp çepni <oguzalpcepni@gmail.com>
2023-07-09 22:00:30 +0300

    Validation

---
+++ Core/CrossCuttingConcerns/Validation/ValidationTool.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.CrossCuttingConcerns.Validation
+{
+    public static class ValidationTool
+    {
+        public static void Validate(IValidator validator)
+        {
+            var context = new ValidationContext<Product>(product);                  // product için doğrulama işlemi yapıcam 
+            ProductValidator productValidator = new ProductValidator();             //benim kurallarım için 
+            var result =  productValidator.Validate(context);           
+            if (!result.IsValid)                                                    // geçerli değilse hata fırlat 
+            {
+                throw new ValidationException(result.Errors);
+            }
+            
+        }
+    }
+}
