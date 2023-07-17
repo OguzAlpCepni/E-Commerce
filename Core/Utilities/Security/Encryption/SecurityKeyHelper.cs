b2ddda1ad47488b9e6245e92514f37efbc8df2dd
oğuz alp çepni <oguzalpcepni@gmail.com>
2023-07-17 18:19:49 +0300

    Security jwt

---
+++ Core/Utilities/Security/Encryption/SecurityKeyHelper.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.Security.Encryption
+{
+    public class SecurityKeyHelper    // işin içerisinde şifreleme olan sistemlerde bizim her şeyi bir byte aray formatında veriyor olmamız lazım 
+    {
+        public static SecurityKey CreateSecurityKey(string securityKey)// appjson.app yazdıgımız securityKey ver ve onu byte çevir simetrik
+        {
+            return new SymmetricSecurityKey(Encoding.UTF8.GetBytes(securityKey)); // ?????
+        }
+    }
+}
